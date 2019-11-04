ec616f7ab1991eb0e970445f376ee50a4508320f
wangyan <wangyan@jlylcf.cn>
2019-11-04 16:09:54 +0800

    拆分商家sagas

---
+++ app/store/sagas/ShangJiaSagas.js
@@ -0,0 +1,25 @@
+import { call, put } from "redux-saga/effects";
+import axios from 'axios'
+import DevicesStorageUtil from '../../util/DeviceStorageUtil';
+import DateUtil from '../../util/DateUtil';
+import { getIsShow as shangjiaGetIsShow, shangjiaJsonData as getShangJia } from "../../components/ShangJia/store/actionCreators";
+
+
+function*   fetchPost (api, params, headers)  {
+
+    let url = 'https://cs.jlcxtx.com/' + api
+    let fetchOptions = {
+        method: 'POST',
+        headers: headers,
+        body: params,
+    };
+    console.log(api + "请求开始", fetchOptions);
+    let res = yield fetch(url, fetchOptions); 
+    let response = yield res.text();
+    yield put(getShangJia(response));
+    yield put(shangjiaGetIsShow(false));
+}
+
+export {
+    fetchPost
+}
--- app/store/sagas/index.js
+++ app/store/sagas/index.js
@@ -1,6 +1,9 @@
 import { takeEvery, put } from "redux-saga/effects";
 import { getAxios } from "./HomeSagas.js";
-
+import {fetchPost} from './ShangJiaSagas';
+import { TEST_JSON } from '../../components/ShangJia/store/actionTypes';
+import DevicesStorageUtil from '../../util/DeviceStorageUtil';
+import DateUtil from '../../util/DateUtil';
 
 
 
@@ -10,6 +13,8 @@ import {  GET_IMGLIST_DATA  } from "../../components/Home/store/actionTypes";
 
 function* mySaga() {
     yield takeEvery(GET_IMGLIST_DATA, HomeImgList) // home组件redux
+    yield takeEvery(TEST_JSON, getShangJiaJSON);//shangjia组件
+
 }
 
 //home组件首页加载数据
@@ -23,6 +28,21 @@ function* HomeImgList() {
     yield getAxios('http://192.168.34.102:8081/public/home/img.json',null)
 
     // yield put (getIsShow)
+}
+
+
+function* getShangJiaJSON(action) {
+    let formData = new FormData();
+    let lastPullTime = yield DevicesStorageUtil.get('lastPullTime');
+    if (lastPullTime == null) {
+        lastPullTime = DateUtil.formatDate(DateUtil.getBeforeDayDate(2).getTime(), 'yyyy-MM-dd hh:mm:ss');
+    }
+    let map = {};
+    formData.append('lastPullTime', lastPullTime)
+    map = { Accept: 'application/json, text/plain,*/*' };
+    yield fetchPost("/store/home", formData, map);
+   
+    DevicesStorageUtil.save("lastPullTime", yield DateUtil.formatDate(new Date().getTime(), 'yyyy-MM-dd hh:mm:ss'));
 }
 
 export default mySaga
