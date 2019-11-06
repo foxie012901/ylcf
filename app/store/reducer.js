21f4b7b2f1bb534def8e75b99ce084a6cb96c904
wangyan <wangyan@jlylcf.cn>
2019-11-06 15:55:02 +0800

    增加login saga

--- app/store/reducer.js
+++ app/store/reducer.js
@@ -3,11 +3,12 @@ import { combineReducers } from "redux-immutable";
 import { todolist } from "../components/TodoList/store";
 import { home } from "../components/Home/store";
 import {shangjia} from "../components/ShangJia/store";
-
+import {login} from "../components/Login/store";
 const reducer = combineReducers({
     home,
     todolist,
-    shangjia
+    shangjia,
+    login
 })
 
 export default reducer
