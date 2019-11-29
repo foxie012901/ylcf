787522aa96c068f241f6293cdcda2c924d6e12d0
wangyan <wangyan@jlylcf.cn>
2019-11-29 10:21:38 +0800

    wy 添加获取网络状态

--- app/components/ChildServicesDetailsTitle/ChildServicesDetailsTitle.js
+++ app/components/ChildServicesDetailsTitle/ChildServicesDetailsTitle.js
@@ -9,9 +9,8 @@ import {
     Platform,
     Dimensions,
     FlatList,
-    Modal,
-    Picker,
-    TouchableOpacity
+    TouchableOpacity,
+     
 } from 'react-native';
 const mWidth = Dimensions.get('window').width;
 const mHeight = Dimensions.get('window').height;
@@ -20,7 +19,9 @@ import { actionCreators } from "./store";
 import { connect } from "react-redux";
 import DeviceInfo from 'react-native-device-info';
 import { getLogIn } from './store/actionCreators';
+import Toast from 'react-native-easy-toast';
 import IconFont from 'react-native-vector-icons/Ionicons'
+import NetInfo from '@react-native-community/netinfo'
  class ChlidServicesDetailsTitle extends Component {
     constructor(props) {
         super(props);
@@ -31,25 +32,38 @@ import IconFont from 'react-native-vector-icons/Ionicons'
 
 
     }
+    componentDidMount(){
+        NetInfo.fetch().then(state => {
+            console.log("Connection type", state.type);
+            console.log("Is connected?", state.isConnected);
+            if(state.type==='wifi'){
+                this.refs.toast.show('当前为wifi');
+            }
+          });
+    }
     componentWillUpdate() {
      
     }
     _carListRenderItem({item,index}){
         console.log(item,index);
-        return(<TouchableOpacity  onPress={()=>{this.props._changeTitle(item.hphm) ,this.props._changeSelectShow(false)}}><View style={{width:200,height:50,position:'relative'}}><Text>{item.hphm}</Text></View></TouchableOpacity>)
+        let sty ={};
+        
+        index===0?sty={width:mWidth*0.25,height:mHeight*0.06,position:'relative'}:sty={width:mWidth*0.25,height:mHeight*0.06,position:'relative',borderTopColor:'#cccccc',borderTopWidth:1}
+
+        return(<TouchableOpacity  onPress={()=>{this.props._changeTitle(item.hphm) ,this.props._changeSelectShow(false)}}><View style={sty}><Text style={{textAlign:'center',lineHeight:mHeight*0.06}}>{item.hphm}</Text></View></TouchableOpacity>)
     }
     render() {
       
      
         return (
                 <View>
-            <View style={{width:mWidth,height:mHeight*0.1,backgroundColor:"#FFFFFF",flexDirection:'row',borderBottomWidth:2,justifyContent:'flex-start'}}>       
+            <View style={{width:mWidth,height:mHeight*0.1,backgroundColor:"#FFFFFF",flexDirection:'row',borderBottomWidth:2,justifyContent:'flex-start',}}>       
                 <View style={{alignSelf:'center',flex:1,justifyContent:'flex-start',flexDirection:'row',}}>
                     <TouchableHighlight onPress={()=>{Actions.pop()}} style={{width:50,height:50}}><Text style={{textAlign:'center',lineHeight:50}}>〈</Text></TouchableHighlight>
                 </View>
-                <TouchableHighlight onPress={()=>{this.props._changeSelectShow(true)}} style={{alignSelf:'center',flex:1}}>
-                <Text style={{textAlign:'center'}}>{this.props.title}</Text>
-        </TouchableHighlight>
+                <TouchableHighlight onPress={()=>{this.props.carList.length>0?this.props._changeSelectShow(true):null}} style={{alignSelf:'center',flex:1}}>
+                    <Text style={{textAlign:'center'}}>{this.props.title}</Text>
+                </TouchableHighlight>
                 <View style={{flex:1,}}></View>
 
             </View>
@@ -57,9 +71,11 @@ import IconFont from 'react-native-vector-icons/Ionicons'
                 <FlatList data={this.props.carList}
                 style={{width:mWidth*0.25,height:mHeight*0.25,backgroundColor:'#ffffff',position:'absolute',zIndex:99,alignSelf:'center',top:mHeight*0.1}}
                 renderItem={this._carListRenderItem.bind(this)}
+               
                 />
                 <TouchableOpacity  onPress={()=>{this.props._changeSelectShow(false)}}><View style={{width:mWidth,height:mHeight,backgroundColor: 'rgba(0,0,0,0.7)',}}></View></TouchableOpacity>
             </View>:null}
+            <Toast ref={'toast'} position={'center'}/>
 
             </View>
             
